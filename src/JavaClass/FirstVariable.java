865f0beebdd594ab950b3bfef3aa4df80d7c5940
knightch <amankumarpandey2397@gmail.com>
2022-10-14 23:35:04 +0530

    creating variable and modified

--- src/JavaClass/FirstVariable.java
+++ src/JavaClass/FirstVariable.java
@@ -5,6 +5,8 @@ public class FirstVariable {
 	public static void main(String[] args) {
 		String message = "initial value";
 		System.out.println(message);
+		message = "Modified value";
+		System.out.println(message);
 
 	}
 
