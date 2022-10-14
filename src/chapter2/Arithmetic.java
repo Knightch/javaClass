f094a99ead35b0bf99f9796f313aa02f7842be49
knightch <amankumarpandey2397@gmail.com>
2022-10-15 00:12:48 +0530

    doing arithmetic

---
+++ src/chapter2/Arithmetic.java
@@ -0,0 +1,22 @@
+package chapter2;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 100;
+		int factor = 20;
+		int sum = 0;
+		
+		sum = num + factor;
+		System.out.println("Addition sum: "+sum);
+		sum = num - factor;
+		System.out.println("Subtraction sum:" +sum);
+		sum = num*factor;
+		System.out.println("Multipication sum:" +sum);
+		sum = num/factor;
+		System.out.println("Division sum:"+sum);
+
+	}
+
+}
