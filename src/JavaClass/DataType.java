b00cdf047911b6bdfc3c020aba2a787d337eca76
knightch <amankumarpandey2397@gmail.com>
2022-10-14 23:45:50 +0530

    recognizing data type

---
+++ src/JavaClass/DataType.java
@@ -0,0 +1,17 @@
+package JavaClass;
+
+public class DataType {
+	public static void main (String[] args) {
+		char letter = 'M';
+		String title = "Java in essay steps";
+		int number = 365;
+		float decimal = 98.6f;
+		boolean result = true;
+		
+		System.out.println("Initial is "+letter);
+		System.out.println("Book is "+title);
+		System.out.println("Days are "+number);
+		System.out.println("Temperatue is "+decimal);
+		System.out.println("Answer is "+result);
+	}
+}
