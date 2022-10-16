f3fe4ad177d325998355bc67a9fbc5113a30d1e0
knightch <amankumarpandey2397@gmail.com>
2022-10-16 16:55:58 +0530

    assigning values

---
+++ src/chapter2/Assignment.java
@@ -0,0 +1,26 @@
+package chapter2;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String txt = "Fantastic";
+		String lang = " Java";
+		txt += lang;
+		System.out.println("Add & Assign String: "+ txt);
+		
+		int sum = 10;
+		int num = 20;
+		sum += num;
+		System.out.println("Add & Assign Integer: "+sum);
+		
+		int factor = 5;
+		sum *= factor;
+		System.out.println("Multipication: "+sum);
+		
+		sum /= factor;
+		System.out.println("Division: "+ sum);
+		
+	}
+
+}
